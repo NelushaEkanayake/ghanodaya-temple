b7a12efb4cb3d7062805dd00c087afb25506a39f
shanipavithra <shanipavithra26@gmail.com>
2021-01-18 08:30:24 -1200

    video header

--- resources/videos/index.php
+++ resources/videos/index.php
@@ -1,7 +1,7 @@
 <section id="recent" class="bg-2">
     <?php
     $st = new site();
-    $st-> pageheading('VIDEOS');
+    $st-> pageheading('වීඩියෝ දර්ශණ');
     ?>
     <div class="container">
         <div class="row">
