dcd504ebc5d9c1696c9a24c212b300cbb7a79a71
hirushani26 <hirupathu98@gmail.com>
2021-01-18 15:47:16 +0530

    indent

--- resources/donation/index.php
+++ resources/donation/index.php
@@ -1,47 +1,48 @@
-<section id ="donation">
-<?php
+<section id="donation">
+    <?php
     $st = new site();
     $st-> pageheading('DONATION');
     ?>
     <div class="container">
 
-    <div class="row">
+        <div class="row">
 
-    <div class="col-md-12  description">
-        <h5 class="d-title ">“දසබලසේලප්පභවා නිබ්බානමහාසමුද්දපරියන්තා අට්ඨංගමග්ගසලිලා ජිනවචනනදී චිරං වහතූති”
+            <div class="col-md-12  description">
+                <h5 class="d-title ">“දසබලසේලප්පභවා නිබ්බානමහාසමුද්දපරියන්තා අට්ඨංගමග්ගසලිලා ජිනවචනනදී චිරං වහතූති”
 
-</h5>
-        <p class="d-text">“දසබලයන් වහන්සේ නමැති ශෛලමය පර්වතයෙන් පැන නැගී, අමා මහ නිවන නම් වූ මහා සාගරය අවසන් කොට ඇති,
-ආර්ය අෂ්ඨාංගික මාර්ගය නම් වූ සිහිල් දිය දහරින් හෙබි,
-උතුම් ශ්‍රී මුඛ බුද්ධවචන ගංගාවෝ (ලෝ සතුන්ගේ සසර දුක නිවාලමින්) බොහෝ කල් ගලා බස්නා සේක්වා!”
-(සළායතන වර්ගය –  උද්දාන ගාථා)</p>
+                </h5>
+                <p class="d-text">“දසබලයන් වහන්සේ නමැති ශෛලමය පර්වතයෙන් පැන නැගී, අමා මහ නිවන නම් වූ මහා සාගරය අවසන් කොට
+                    ඇති,
+                    ආර්ය අෂ්ඨාංගික මාර්ගය නම් වූ සිහිල් දිය දහරින් හෙබි,
+                    උතුම් ශ්‍රී මුඛ බුද්ධවචන ගංගාවෝ (ලෝ සතුන්ගේ සසර දුක නිවාලමින්) බොහෝ කල් ගලා බස්නා සේක්වා!”
+                    (සළායතන වර්ගය – උද්දාන ගාථා)</p>
 
-</div>
-      </div>
-      <div class="row">
+            </div>
+        </div>
+        <div class="row">
 
-    <div class="col-md-4 p-2">
-      <img src="asset/img/donation/20180319_103340.jpg" class="d-img" alt="...">
+            <div class="col-md-4 p-2">
+                <img src="asset/img/donation/20180319_103340.jpg" class="d-img" alt="...">
+            </div>
+
+            <div class="col-md-4 p-2">
+                <img src="asset/img/donation/IMG_20170928_172221.jpg" class="d-img" alt="...">
+            </div>
+
+            <div class="col-md-4 p-2">
+                <img src="asset/img/donation/20200107_074916.jpg" class="d-img" alt="...">
+            </div>
+        </div>
+
+
+        <div class="row text-center py-4">
+            <div class="col-md-12  btn">
+                <button>DONATE</button>
+            </div>
+        </div>
+
+
+
     </div>
-
-    <div class="col-md-4 p-2">
-      <img src="asset/img/donation/IMG_20170928_172221.jpg" class="d-img" alt="...">
-    </div>
-
-    <div class="col-md-4 p-2">
-      <img src="asset/img/donation/20200107_074916.jpg" class="d-img" alt="...">
-    </div>
-    </div>
-    
-   
-      <div class="row text-center py-4">
-      <div class="col-md-12  btn">
-      <button>DONATE</button>
-      </div>
-      </div>
-      
-   
-
-</div>
 
 </section>
