d673de9adfc60f3dce40e46d6f4683f55d8be896
sanduni <schathurika98@gmail.com>
2021-01-18 14:49:50 +0530

    Update index.php

--- resources/news/index.php
+++ resources/news/index.php
@@ -8,64 +8,68 @@
         <div class="col">
             <div class="card">
                 <div class="card-img">
-                    <img src="asset/img/news/news.jpg" class="card-img-top" alt="...">
+                    <img src="asset/img/news/1.jpg" class="card-img-top" alt="...">
                 </div>
             <div class="card-body">
-                <h5 class="card-title">අභයදානමය පුණ්‍යකර්මය.</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+                <h5 class="card-title p-0">අභයදානමය පුණ්‍යකර්මය.</h5>
+                <p class="news_DateField">Desember 15th 2021</p>
+                <p class="card-text"> පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය හේතුවෙන් පුද්ගලික තොරතුරු ලබා දෙන ලෙස ඉල්ලා සිටියි පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය .</p>
                 <h6 class="detail">
-                       <span class="news_DateField">03-02-2019</span>    
-                    
-                    </h6>
+                    <span class="more">තව යන්න</span>    
+                </h6>
             </div>
             </div>
         </div>
         <div class="col">
             <div class="card">
                 <div class="card-img">
-                    <img src="asset/img/news/news.jpg" class="card-img-top" alt="...">
-                    
+                    <img src="asset/img/news/1.jpg" class="card-img-top" alt="...">
+
                 </div>
-            <div class="card-body">
-                <h5 class="card-title">අභයදානමය පුණ්‍යකර්මය.</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <h6 class="detail">
-                       <span class="news_DateField">03-02-2019</span>    
-                    
-                    </h6>
+                <div class="card-body">
+
+            <h5 class="card-title p-0">අභයදානමය පුණ්‍යකර්මය.</h5>
+            <p class="news_DateField">Desember 15th 2021</p>
+            <p class="card-text"> පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය හේතුවෙන් පුද්ගලික තොරතුරු ලබා දෙන ලෙස ඉල්ලා සිටියි පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය .</p>
+            <h6 class="detail">
+                <span class="more">තව යන්න</span>    
+            </h6>
             </div>
             </div>
         </div>
         <div class="col">
             <div class="card">
                 <div class="card-img">
-                    <img src="asset/img/news/news.jpg" class="card-img-top" alt="...">
-                   
-                </div>
-            <div class="card-body">
-                <h5 class="card-title">අභයදානමය පුණ්‍යකර්මය.</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <h6 class="detail">
-                       <span class="news_DateField">03-02-2019</span>    
+                    <img src="asset/img/news/1.jpg" class="card-img-top" alt="...">
                     
-                    </h6>
-            </div>
+                </div>
+                <div class="card-body">
+
+                <h5 class="card-title p-0">අභයදානමය පුණ්‍යකර්මය.</h5>
+                <p class="news_DateField">Desember 15th 2021</p>
+                <p class="card-text"> පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය හේතුවෙන් පුද්ගලික තොරතුරු ලබා දෙන ලෙස ඉල්ලා සිටියි පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය .</p>
+                <h6 class="detail">
+                    <span class="more">තව යන්න</span>    
+                </h6>
+                </div>
             </div>
         </div>
         <div class="col">
             <div class="card">
                 <div class="card-img">
-                    <img src="asset/img/news/news.jpg" class="card-img-top" alt="...">
-                   
-                </div>
-            <div class="card-body">
-                <h5 class="card-title">අභයදානමය පුණ්‍යකර්මය.</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <h6 class="detail">
-                       <span class="news_DateField">03-02-2019</span>    
+                    <img src="asset/img/news/1.jpg" class="card-img-top" alt="...">
                     
-                    </h6>
-            </div>
+                </div>
+                <div class="card-body">
+
+                <h5 class="card-title p-0">අභයදානමය පුණ්‍යකර්මය.</h5>
+                <p class="news_DateField">Desember 15th 2021</p>
+                <p class="card-text"> පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය හේතුවෙන් පුද්ගලික තොරතුරු ලබා දෙන ලෙස ඉල්ලා සිටියි පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය පෙළ විභාගය සහ ශිෂ්‍යත්ව විභාගය සඳහා වර්තමාන කිරීටක වයිරස තත්ත්වය .</p>
+                <h6 class="detail">
+                    <span class="more">තව යන්න</span>    
+                </h6>
+                </div>
             </div>
         </div>
         </div>
