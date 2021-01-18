43fbe0b653143d54cf841b23503099e0d5676a66
KasunV <kasungayasiri@gmail.com>
2021-01-18 15:53:51 +0530

    Update index.php

--- resources/videos/index.php
+++ resources/videos/index.php
@@ -1,22 +1,26 @@
-<section id = "recent">
+<section id="recent">
     <?php
     $st = new site();
     $st-> pageheading('VIDEOS');
     ?>
-<div class="container">
-  <div class="row">
-    <div class="col-md-4">
-    <iframe  src="https://www.youtube.com/embed/5NtBmOqESo4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-       
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <iframe class="w-100" src="https://www.youtube.com/embed/5NtBmOqESo4" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+
+            </div>
+            <div class="col-md-4">
+                <iframe class="w-100" src="//www.youtube.com/embed/0GfCP5CWHO0" frameborder="0"
+                    allowfullscreen></iframe>
+
+            </div>
+            <div class="col-md-4">
+                <iframe class="w-100" src="//www.youtube.com/embed/0GfCP5CWHO0" frameborder="0"
+                    allowfullscreen></iframe>
+
+            </div>
+        </div>
     </div>
-    <div class="col-md-4">
-    <iframe  src="//www.youtube.com/embed/0GfCP5CWHO0" frameborder="0" allowfullscreen></iframe>
-  
-    </div>
-    <div class="col-md-4">
-<iframe  src="//www.youtube.com/embed/0GfCP5CWHO0" frameborder="0" allowfullscreen></iframe> 
-    
-    </div>
-  </div>
-</div>
-    </section>
+</section>
