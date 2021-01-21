d9d2d7c770fa52bd7a20ac1cd1259b6c6339de26
kalana797 <kalana797@gmail.com>
2021-01-21 13:35:59 +0530

    gallery

--- resources/gallery/index.php
+++ resources/gallery/index.php
@@ -6,16 +6,17 @@
     <div class="container">
 
         <div class="row">
+            
             <div class="col-md-4">
-                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img
+                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img class="hover-shadow cursor"
                         src="asset/img/gallery/gallary3.jpg" alt=""></a>
             </div>
             <div class="col-md-4">
-                <a class="test-popup-link" href="asset/img/gallery/gallary1.jpg"> <img
+                <a class="test-popup-link" href="asset/img/gallery/gallary1.jpg"> <img class="hover-shadow cursor"
                         src="asset/img/gallery/gallary1.jpg" alt=""></a>
             </div>
             <div class="col-md-4">
-                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img
+                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img class="hover-shadow cursor"
                         src="asset/img/gallery/gallary3.jpg" alt=""></a>
             </div>
 
