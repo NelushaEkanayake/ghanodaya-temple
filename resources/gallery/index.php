69f19a84c39cea12f2e7ed4412963bf91fbce1f4
kalana797 <kalana797@gmail.com>
2021-01-21 13:44:39 +0530

    gallery page and css

--- resources/gallery/index.php
+++ resources/gallery/index.php
@@ -8,15 +8,15 @@
         <div class="row">
             
             <div class="col-md-4">
-                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img class="hover-shadow cursor"
+                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img class="img-gal-home"
                         src="asset/img/gallery/gallary3.jpg" alt=""></a>
             </div>
             <div class="col-md-4">
-                <a class="test-popup-link" href="asset/img/gallery/gallary1.jpg"> <img class="hover-shadow cursor"
+                <a class="test-popup-link" href="asset/img/gallery/gallary1.jpg"> <img class="img-gal-home"
                         src="asset/img/gallery/gallary1.jpg" alt=""></a>
             </div>
             <div class="col-md-4">
-                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img class="hover-shadow cursor"
+                <a class="test-popup-link" href="asset/img/gallery/gallary3.jpg"> <img class="img-gal-home"
                         src="asset/img/gallery/gallary3.jpg" alt=""></a>
             </div>
 
