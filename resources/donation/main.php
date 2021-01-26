a3e07643865eca5cde7e9a30807ed48ba607c8dc
hirushani26 <hirupathu98@gmail.com>
2021-01-26 16:07:04 +0530

    newcss

--- resources/donation/main.php
+++ resources/donation/main.php
@@ -1,10 +1,21 @@
 <section id="sponcers">
     <div class="container">
-        <div class="row ">
+        <div class="row masssege">
+      
             <p class="title">පින් කරනු කැමති අයට පින්බර අවස්ථාවක්</p>
             <hr>
+            <i class="fas fa-dharmachakra"></i>
             <p class="title">තෙරුවන් සරණයි!</p>
             <p class="introduction">ඒ භාග්‍යවත් වූ අර්හත් වූ
+                සම්මා සම්බුදුරජාණන් වහන්සේගේ
+                ශ‍්‍රීමත් සර්වඥ ධාතූන් වහන්සේලා ද
+                සම්බුදු රජුන්ගේ මහා ශ‍්‍රාවකයන් වහන්සේ නමක් වූ,
+                අපමණ තේජෝ බල ඇති, අපමණ අනුකම්පා ඇති,
+                ආර්ය වූ අපගේ අංගුලිමාල මහරහතන් වහන්සේගේ
+                ශ‍්‍රීමත් ධාතූන් වහන්සේලා ද
+                වඩාහිඳුවීමෙන් සුපූජනීය වූ,
+                මහා තෙද, මහා බල ආශිර්වාදය පැතිරුණ
+                ඒ භාග්‍යවත් වූ අර්හත් වූ
                 සම්මා සම්බුදුරජාණන් වහන්සේගේ
                 ශ‍්‍රීමත් සර්වඥ ධාතූන් වහන්සේලා ද
                 සම්බුදු රජුන්ගේ මහා ශ‍්‍රාවකයන් වහන්සේ නමක් වූ,
@@ -15,13 +26,13 @@
                 මහා තෙද, මහා බල ආශිර්වාදය පැතිරුණ</p>
         </div>
 
-        <div class="row mt-5">
+        <div class="row pt-5 mt-5">
             <div class="col-md-8 p-2">
-                <img src="asset/img/donation/2.jpg" class="d-img" alt="...">
+                <img src="asset/img/2.jpg" class="d-img" alt="...">
             </div>
             <div class="col-md-4 p-2">
-                <img src="asset/img/donation/3.jpg" class="d-img pb-2">
-                <img src="asset/img/donation/4.jpg" class="d-img" alt="...">
+                <img src="asset/img/3.jpg" class="d-img mb-2">
+                <img src="asset/img/4.jpg" class="d-img" alt="...">
             </div>
         </div>
      </div>
@@ -31,11 +42,11 @@
 
 <section id="donations">
 
-    <div class="col-md-12">
+    <div class="col-md-12 background p-5">
 
-        <p class="donate  pt-5"> ඥානෝදය විහාරස්ථානයේ ඉදිකිරීම් සදහා පින්වත් ඔබගේ ආධාර පහත ගිණුමට බැර කළ හැකිය</p>
+        <p class="donate pt-3"> ඥානෝදය විහාරස්ථානයේ ඉදිකිරීම් සදහා පින්වත් ඔබගේ ආධාර පහත ගිණුමට බැර කළ හැකිය</p>
 
-        <div class="col-md-12 py-5 account">
+        <div class="col-md-12 pt-3 account">
             <p> ඥානෝදය සංවර්ධන අරමුදල</p>
             <p>ගිණුම් අංකය</p>
             <p class="number">
@@ -45,5 +56,33 @@
             <p>සම්පත් බැංකුව, හෙට්ටිපොළ ශාඛාව.</p>
         </div>
     </div>
+
+  
+    <div class="container spon">
+    <div class="row ">
+<p class="name p-5"> ඥානෝදය විහාරස්ථානයේ ඉදිකිරීම් සදහා පින්වත් ඔබගේ ආධාර පහත ගිණුමට බැර කළ හැකිය</p>
+
+<div class="col-md-3">
+<img src="asset/img/5.png " class="logo m-2">
+</div>
+
+<div class="col-md-3">
+<img src="asset/img/5.png " class="logo  m-2">
+</div>
+
+<div class="col-md-3">
+<img src="asset/img/5.png " class="logo m-2">
+</div>
+
+<div class="col-md-3">
+<img src="asset/img/5.png " class="logo m-2">
+</div>
+</div>
+
+</div>
+
+
+
 </section>
+
 
