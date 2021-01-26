8ada23278835907aa3fb99fe55e4376ccc00b338
Ayesha Dissanayaka <77577000+ayeshz-3995@users.noreply.github.com>
2021-01-26 16:06:56 +0530

    m

--- resources/main.php
+++ resources/main.php
@@ -72,14 +72,14 @@ elseif($_GET['tag'] == 'preschool'){
 
 elseif($_GET['tag'] == 'gallery'){
     require_once('resources/gallery/main.php');
-
+}
 elseif($_GET['tag'] == 'contact'){
     require_once('resources/contac/contac.php');
 }
 elseif($_GET['tag'] == 'sponcers'){
     require_once('resources/donation/main.php');
 
-
+}
 require_once('resources/footer/index.php');
 ?>
 
