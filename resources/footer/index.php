3e082c01628b5609dc4bee235ea292a89d643f10
Ayesha Dissanayaka <77577000+ayeshz-3995@users.noreply.github.com>
2021-02-01 11:57:53 +0530

    m

--- resources/footer/index.php
+++ resources/footer/index.php
@@ -44,73 +44,50 @@
                     </div>
                 </section>
             </div>
+            <div class="col-12 col-md-3">
+                <section class="widget">
+
+                    <div class="single_leftbar wow fadeInDown">
+                        <div class="widget-heading">
+                            <h4>සම්බන්දවන්න</h4>
+                        </div>
+                        <div class="footer-contact-info">
+                            <ul class="label_nav">
+                                <li><i class="fas fa-building"></i>ඥානෝදය පන්සල </li>
+                                <li><i class="fas fa-map-marker"></i>අංක 01, තලාව පාර, අනුරාධපුර</li>
+                                <li><i class="fas fa-envelope"></i><a href="ිි:mail@example.com">mail@example.com</a>
+                                </li>
+                                <li><i class="fas fa-phone"></i>(025) 456-7890 <span>|</span> 125-668-886 </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="col-12 col-md-3">
+                <section class="widget">
+
+                    <div class="single_leftbar wow fadeInDown">
+                        <div class="widget-heading">
+                            <h4>සබැදිය</h4>
+
+                        </div>
+                        <div class="singleleft_inner">
+                            <ul class="link_nav">
+                                <li><a href="#">Temple Overview</a></li>
+                                <li><a href="#">දඔුල්ලේ සුමන්ගල හිමි</a></li>
+                                <li><a href="#">ඡායාරූප</a></li>
+                                <li><a href="#">twitter</a></li>
+                                <li><a href="#">Facebook </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </div>
             <a class="scrollToTop" href="#"><i class="fas fa-angle-up"></i></a>
     </footer>
 </section>
-<section id="foot2" class="col-md-12 row m-0 pt-3">
 
-    <div class="col-12 col-md-6 footer_bottom_left">
-
-        <p>&copy; By : D.M.A.M. Dissanyaka | 2020-December. &middot; <a href="#">Privacy</a> &middot; <a
-                href="#">Terms</a></p>
-    </div>
-    <div class="col-12 col-md-6 footer_bottom_right">
-        <ul>
-            <li><a class="btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" href="#"><i
-                        class="fab fa-twitter"></i></a></li>
-            <li><a class="btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" href="#"><i
-                        class="fab fa-facebook"></i></a></li>
-            <li><a class="btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Googel+" href="#"><i
-                        class="fab fa-google-plus"></i></a></li>
-            <li><a class="btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Youtube" href="#"><i
-                        class="fab fa-youtube"></i></a></li>
-            <li><a class="btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Rss" href="#"><i
-                        class="fas fa-rss"></i></a></li>
-        </ul>
-    </div>
-</section>
-<div class="col-12 col-md-3">
-    <section class="widget">
-
-        <div class="single_leftbar wow fadeInDown">
-            <div class="widget-heading">
-                <h4>සබැදිය</h4>
-
-            </div>
-            <div class="singleleft_inner">
-                <ul class="link_nav">
-                    <li><a href="#">Temple Overview</a></li>
-                    <li><a href="#">දඔුල්ලේ සුමන්ගල හිමි</a></li>
-                    <li><a href="#">ඡායාරූප</a></li>
-                    <li><a href="#">twitter</a></li>
-                    <li><a href="#">Facebook </a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
-</div>
-<div class="col-12 col-md-3">
-    <section class="widget">
-
-        <div class="single_leftbar wow fadeInDown">
-            <div class="widget-heading">
-                <h4>සබැදිය</h4>
-
-            </div>
-            <div class="singleleft_inner">
-                <ul class="link_nav">
-                    <li><a href="#">Temple Overview</a></li>
-                    <li><a href="#">දඔුල්ලේ සුමන්ගල හිමි</a></li>
-                    <li><a href="#">ඡායාරූප</a></li>
-                    <li><a href="#">twitter</a></li>
-                    <li><a href="#">Facebook </a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
-</div>
-</div>
-</section>
 
 <section id="foot2" class="col-md-12 row m-0">
     <div class="col-12 col-md-6 footer_bottom_left">
@@ -128,5 +105,3 @@
         </ul>
     </div>
 </section>
-<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
-</footer>
