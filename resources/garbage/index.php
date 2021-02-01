1648f566d5fac134963819ced142941a5a0b0af1
sanduni <schathurika98@gmail.com>
2021-02-01 13:03:31 +0530

    garbage

--- resources/garbage/index.php
+++ resources/garbage/index.php
@@ -1,48 +1,32 @@
-
 <section id="garbage">
     <div class="container">
-
         <div class="row">
-<<<<<<< Updated upstream
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body-lab">
-                        <h5 class="card-title">කසල</h5>
-                        <p class="card-text">රාජකීය වනාහි – ලාංකීය ධරණිතලයේ සෙසු පාසල් අතර පොලොන්නරුව රාජකීය
-                            විද්‍යාලයට සුවිශේෂි වටිනාකමක් හිමි වී ඇත්තේ ගුරු
-                            – සිසු දේපාර්ශවය තුලම , අතීතයේ පටන් අද දක්වාම නොනැසී පවතින
-                            හර
-                            පද්ධතිය
-                            සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්
-                            හර
-                            පද්ධතිය
-                            සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්හර
-                            පද්ධතිය
-                            සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්
-                            හර
-                            පද්ධතිය
-                            සහ සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්හර
-                            පද්ධතිය
-                            සහ  අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්
-                        </p>
-                        <button class="button button5">වැඩිදුර තොරතුරු</button>
-                    </div>
-=======
             <div class="col-md-6">
                 <div class="garbage">
-                    <img class="img" src="asset/img/garbage/2.jpg" alt="" width="100%">
->>>>>>> Stashed changes
+                    <img class="img" src="asset/img/2.jpg" alt="" width="100%">
                 </div>
             </div>
-
-            <div class="col-sm-6">
-                <div class="garbage">
-                    <img class="img" src="asset/img/garbage/1.jpg" alt="" width="100%">
+            <div class="col-md-6 align-self-center ">
+                <h5 class="title">කසල </h5>
+                <p class="text ">රාජකීය වනාහි – ලාංකීය ධරණිතලයේ සෙසු පාසල් අතර පොලොන්නරුව රාජකීය
+                    විද්‍යාලයට සුවිශේෂි වටිනාකමක් හිමි වී ඇත්තේ ගුරු
+                    – සිසු දේපාර්ශවය තුලම , අතීතයේ පටන් අද දක්වාම නොනැසී පවතින
+                </p>
+                <button class="button button5">වැඩිදුර තොරතුරු</button>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="card-group" width="100%">
+                <div class="card p-3">
+                    <img src="./asset/img/3.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card p-3">
+                    <img src="./asset/img/3.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card p-3">
+                    <img src="./asset/img/3.jpg" class="card-img-top" alt="...">
                 </div>
             </div>
         </div>
