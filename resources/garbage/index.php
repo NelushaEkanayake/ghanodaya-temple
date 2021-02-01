15c84145e33bb99491298b23e4ed181b418df8fc
sanduni <schathurika98@gmail.com>
2021-02-01 13:38:45 +0530

    garbage index

--- resources/garbage/index.php
+++ resources/garbage/index.php
@@ -1,7 +1,6 @@
 <section id="garbage">
     <div class="container">
         <div class="row">
-<<<<<<< HEAD
             <div class="col-md-6">
                 <div class="garbage">
                     <img class="img" src="asset/img/2.jpg" alt="" width="100%">
@@ -28,47 +27,8 @@
                 </div>
                 <div class="card p-3">
                     <img src="./asset/img/3.jpg" class="card-img-top" alt="...">
-=======
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body-lab">
-                        <h5 class="card-title">කසල</h5>
-                        <p class="card-text">රාජකීය වනාහි – ලාංකීය ධරණිතලයේ සෙසු පාසල් අතර පොලොන්නරුව රාජකීය
-                            විද්‍යාලයට සුවිශේෂි වටිනාකමක් හිමි වී ඇත්තේ ගුරු
-                            – සිසු දේපාර්ශවය තුලම , අතීතයේ පටන් අද දක්වාම නොනැසී පවතින
-                            හර
-                            පද්ධතිය
-                            සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්
-                            හර
-                            පද්ධතිය
-                            සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්හර
-                            පද්ධතිය
-                            සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්
-                            හර
-                            පද්ධතිය
-                            සහ සහ සුවිශේෂි අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්හර
-                            පද්ධතිය
-                            සහ අනන්‍යතාව හේතුවෙනි . අප විද්‍යාලයට ආවේණික වු මෙම හර පද්ධතිය අතීතයත් ,
-                            වර්තමානයත්
-                        </p>
-                        <button class="button button5">වැඩිදුර තොරතුරු</button>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="garbage">
-                            <img class="img" src="asset/img/garbage/2.jpg" alt="" width="100%">
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6">
-                        <div class="garbage">
-                            <img class="img" src="asset/img/garbage/1.jpg" alt="" width="100%">
-                        </div>
-                    </div>
->>>>>>> ed6568b83f6c58c7b1f0456d1ef97e37780d1d50
                 </div>
             </div>
+        </div>
+    </div>
 </section>
